3463269b6a4b710c372f477e31182337c7aa3922
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-24 21:54:30 +0200

    fix_bag_fetch

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '25376419-21146ccb6c229e91e9e9974ae';
-const PER_PAGE = 40;
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export default async function fetchImages(request, page) {
+  const options = {
+    params: {
+      key: '25376419-21146ccb6c229e91e9e9974ae',
+      q: `${request}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: `${page}`,
+      per_page: 40,
+    },
+  };
+
   try {
-    const response = await axios(
-      `${BASE_URL}?key=${KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`,
-    );
+    const response = await axios(`/`, options);
     return response.data;
   } catch (error) {
     return {
