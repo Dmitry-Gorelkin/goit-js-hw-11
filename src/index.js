f11b0c695ca62750010089306967d96c267d3720
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-24 21:13:31 +0200

    fix_css

--- src/index.js
+++ src/index.js
@@ -74,13 +74,13 @@ function galleryRender(arr) {
       return `<div class="photo-card">
           <a class="link" href="${largeImageURL}"
             ><img class="photo-card-img" src="${webformatURL}" alt="${tags}" loading="lazy"
-          /></a>
+          />
           <div class="photo-card-info">
             <p class="info-item"><b>Likes</b> ${likes}</p>
             <p class="info-item"><b>Views</b> ${views}</p>
             <p class="info-item"><b>Comments</b> ${comments}</p>
             <p class="info-item"><b>Downloads</b> <span>${downloads}</span></p>
-          </div>
+          </div></a>
         </div>`;
     })
     .join('');
